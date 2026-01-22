92361ab4d0b54eb440a48e13fe8bd5b7b0a41b0b
MrPrince998 <peincerana234@gmail.com>
2026-01-22 14:24:18 +0545

    init

---
+++ navigation/my-favorite.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const MyFavoriteScreen = () => {
+  return (
+    <View>
+      <Text>MyFavoriteScreen</Text>
+    </View>
+  )
+}
+
+export default MyFavoriteScreen
